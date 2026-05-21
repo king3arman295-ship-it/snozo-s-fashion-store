e4c237d1cb0bbffc643ecebbf34d1cee292a1744
Safi <king3arman.295@gmail.com>
2026-05-21 00:32:46 -0700

    fix Order model import case issue

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const Order = require("../models/Order")
+const Order = require("../models/order")
 const nodemailer = require("nodemailer")
 
 // =========================
